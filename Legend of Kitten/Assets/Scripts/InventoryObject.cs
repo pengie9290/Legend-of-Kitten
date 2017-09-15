fc7d8b3c2b2eb9865ce47804619b8581f5a465d2
jh5293 <jh5293@nyu.edu>
2017-09-15 19:12:30 -0400

    Created basic player controls and began work on inventory system

---
+++ Legend of Kitten/Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour {
+	public string Name = "";
+	public string PickupMessage = "";
+	public bool IsVisible = true;
+	public bool IsEnabled = true;
+	public bool CanCarry = true;
+	public bool IsCarried = false;
+	public bool IsExpendable = true;
+	public string StateToSet = "";
+	private GameObject ThePlayer;
+
+	void Start () {
+		if (IsVisible == false) {
+			//disable Mesh Renderer
+		}
+		ThePlayer = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	void Update () {
+		
+	}
+
+	public void OnCollisionEnter (Collision collision){
+		if (ThePlayer != null && collision.gameObject == ThePlayer) {
+			if (CanCarry == true) {
+				ThePlayer.SendMessage ("PickUp", Name);
+				IsCarried = true;
+				CanCarry = false;
+			}
+			if (PickupMessage.Length > 0) {
+				ThePlayer.SendMessage ("Report", PickupMessage);
+			}
+			if (StateToSet.Length > 0) {
+				ThePlayer.SendMessage ("SetState", StateToSet);
+			}
+			if (IsExpendable == true) {
+				gameObject.SetActive (false);
+			}
+		}
+	}
+}
---
+++ Legend of Kitten/Assets/Scripts/KittenPlayerControl.cs
@@ -0,0 +1,66 @@
+﻿//Create functions for Pickup and SetState
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittenPlayerControl : MonoBehaviour {
+	public float MaxSpeed = 1f;
+	public int Direction = 0;
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	void Update () {
+		var haxis = Input.GetAxis ("Horizontal");
+		var vaxis = Input.GetAxis ("Vertical");
+		transform.Translate (haxis * MaxSpeed * Time.deltaTime, vaxis * MaxSpeed * Time.deltaTime, 0);
+		Direction = DirectionDetection (haxis, vaxis);
+	}
+
+	int DirectionDetection (float haxis, float vaxis){
+		if (haxis > 0.25f) {
+			if (vaxis > 0.25f) {
+				return 1;
+			} else if (vaxis < -0.25f) {
+				return 3;
+			} else {
+				return 2;
+			}
+		} else if (haxis < -0.25f) {
+			if (vaxis > 0.25f) {
+				return 7;
+			} else if (vaxis < -0.25f) {
+				return 5;
+			} else {
+				return 6;
+			}
+		} else {
+			if (vaxis > 0.25f) {
+				return 8;
+			} else if (vaxis < -0.25f) {
+				return 4;
+			} else {
+				return 0;
+			}
+		}
+	}
+	//0 = still
+	//1 = up-right
+	//2 = right
+	//3 = down-right
+	//4 = down
+	//5 = down-left
+	//6 = left
+	//7 = up-left
+	//8 = up
+
+	public void Report (string Message){
+		print (Message);
+	}
+}
