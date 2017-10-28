ec19ec6cc45239fd0f14e7c5e263f16fddf66274
jh5293 <jh5293@nyu.edu>
2017-10-28 19:54:57 -0400

    Created buttload of pixel art
    -Foyer's Umbrella Stand and Coat Rack (needs redoing)
    -Entrance Room's Counter, Hostess Stand, and Benches (need extra details)
    -Dining Rooms' Tables and Chairs
    -All Four Rooms' Flooring

---
+++ Legend of Kitten/Assets/Scripts/RandomPlacement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class RandomPlacement : MonoBehaviour
+{
+	public List<Transform> Locations;
+
+	void Start()
+	{
+		if (Locations != null)
+		{
+			var Count = Locations.Count;
+			switch (Count)
+			{
+				case 0: break;
+				case 1: transform.position = Locations[0].position;
+					break;
+				default: RandomlyPlace();
+					break;
+			}
+		}
+	}
+	
+	void RandomlyPlace ()
+	{
+		var Count = Locations.Count;
+		int Index = Random.Range(0, Count - 1);
+		print(Index);
+		transform.position = Locations[Index].position;
+	}
+}
